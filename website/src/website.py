8a99e5aec99c87e30c50d5859320c6ef2f64a8f4
Filip Gregor <filip.gregor98@gmail.com>
2023-08-04 22:14:54 +0200

    Articles

--- website/src/website.py
+++ website/src/website.py
@@ -1,13 +1,20 @@
+import os
+
 from flask import Blueprint
 from flask import render_template
 from flask import Flask
 from flask import request
+from flask_sqlalchemy import SQLAlchemy
+
+from markupsafe import escape
 
 import requests
 
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from logging.config import dictConfig
+
+import models
 
 dictConfig({
     'version': 1,
@@ -27,11 +34,25 @@ dictConfig({
 
 bp = Blueprint("website", __name__)
 
+db = SQLAlchemy()
+
 app = Flask(__name__)
+
+db_uri = os.getenv("DB_URI")
+if db_uri is None:
+    print("DB_URI env variable not set")
+    exit(1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DB_URI')
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 app.wsgi_app = ProxyFix(
     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )
+os.getenv('DB_URI')
 
 
 @app.route("/")
@@ -42,6 +63,16 @@ def hello_world():
 @app.route("/code")
 def python_interpreter():
     return render_template("website/code.html")
+
+
+@app.route("/blog")
+def blog_all():
+    return render_template("website/blog_all.html")
+
+
+@app.route("/blog/<string:slug>")
+def blog_view(slug: str):
+    return render_template("website/blog.html", article_slug=escape(slug))
 
 
 @app.route("/run-code", methods=["POST"])
