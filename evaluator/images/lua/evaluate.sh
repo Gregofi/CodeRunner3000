73199470ca3f4a5a89f49439df5cbad7925391f0
Filip Gregor <filip.gregor98@gmail.com>
2023-08-26 15:34:06 +0200

    fix timeout not being properly reported because the -e flag.

--- evaluator/images/lua/evaluate.sh
+++ evaluator/images/lua/evaluate.sh
@@ -1,12 +1,17 @@
-#!/bin/sh -e
+#!/bin/sh
 
 TIMEOUT_EXIT_CODE=137
 
-cd /eval_env
+set -e
+cd /eval_env 
 # Run the actual code as non-root user
 adduser -D evaluator
+set +e
+
 timeout -s KILL ${EVALUATOR_TIMEOUT:-5} sudo -u evaluator lua5.1 /usr/bin/evaluate.lua source.lua > stdout.txt 2> stderr.txt
-if [ $? -eq ${TIMEOUT_EXIT_CODE} ]; then
+exit_code=${?}
+echo "Program exit code ${exit_code}"
+if [ ${exit_code} -eq ${TIMEOUT_EXIT_CODE} ]; then
     echo "The program timeouted"
     echo "The program timeouted" > stderr.txt
 fi
