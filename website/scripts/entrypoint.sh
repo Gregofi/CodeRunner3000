a14783f18bd9b0a663f651249cd567dab1c42725
Filip Gregor <filip.gregor98@gmail.com>
2023-09-10 18:50:23 +0200

    make website port an environment variable

--- website/scripts/entrypoint.sh
+++ website/scripts/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+envsubst '${WEBSITE_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+echo "Starting nginx on port ${WEBSITE_PORT}"
 service nginx restart
 gunicorn -w 4 website:app
