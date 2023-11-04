d7181d7a91283c0c25388bb323eb370adbfb4254
Filip Gregor <filip.gregor98@gmail.com>
2023-11-04 22:58:40 +0100

    chore: run format.

--- website/src/lib/monaco/MonacoEditor.svelte
+++ website/src/lib/monaco/MonacoEditor.svelte
@@ -1,57 +1,57 @@
 <script lang="ts">
-    // Credits from this setup to https://www.codelantis.com/blog/sveltekit-monaco-editor.
-    import { onMount, onDestroy } from 'svelte';
-    import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
+	// Credits from this setup to https://www.codelantis.com/blog/sveltekit-monaco-editor.
+	import { onMount, onDestroy } from 'svelte';
+	import type * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
-    let editor: Monaco.editor.IStandaloneCodeEditor;
-    let monaco: typeof Monaco;
-    function getEditorValue() {
-        return editor.getValue();
-    }
-    let editorContainer: HTMLElement;
-    let editorParent: HTMLDivElement;
+	let editor: Monaco.editor.IStandaloneCodeEditor;
+	let monaco: typeof Monaco;
+	function getEditorValue() {
+		return editor.getValue();
+	}
+	let editorContainer: HTMLElement;
+	let editorParent: HTMLDivElement;
 
-    onMount(async () => {
-        monaco = (await import('./monaco')).default;
+	onMount(async () => {
+		monaco = (await import('./monaco')).default;
 
-        editor = monaco.editor.create(editorContainer);
-        const model = monaco.editor.createModel(
-            [
-                'local function fact(n)',
-                '    if n == 0 then',
-                '        return 1',
-                '    else',
-                '        return n * fact(n - 1)',
-                '    end',
-                'end',
-                '',
-                'print(fact(5))',
-            ].join('\n'), 
-            'lua'
-        );
-        editor.setModel(model);
+		editor = monaco.editor.create(editorContainer);
+		const model = monaco.editor.createModel(
+			[
+				'local function fact(n)',
+				'    if n == 0 then',
+				'        return 1',
+				'    else',
+				'        return n * fact(n - 1)',
+				'    end',
+				'end',
+				'',
+				'print(fact(5))'
+			].join('\n'),
+			'lua'
+		);
+		editor.setModel(model);
 
-        // A hacky? workaround to resize the editor with the page,
-        // since Monaco doesn't do this automatically.
-        // We could use 'automaticLayout' config option but that
-        // only works when making the page bigger.
-        window.addEventListener("resize", () => {
-            editor?.layout({ width: 0, height: 0 });
-            // Resize when the frame refreshes
-            window.requestAnimationFrame(() => {
-                editor.layout(editorParent.getBoundingClientRect());
-            });
-        });
-    });
+		// A hacky? workaround to resize the editor with the page,
+		// since Monaco doesn't do this automatically.
+		// We could use 'automaticLayout' config option but that
+		// only works when making the page bigger.
+		window.addEventListener('resize', () => {
+			editor?.layout({ width: 0, height: 0 });
+			// Resize when the frame refreshes
+			window.requestAnimationFrame(() => {
+				editor.layout(editorParent.getBoundingClientRect());
+			});
+		});
+	});
 
-    onDestroy(() => {
-        monaco?.editor.getModels().forEach((model) => model.dispose());
-        editor?.dispose();
-    });
+	onDestroy(() => {
+		monaco?.editor.getModels().forEach((model) => model.dispose());
+		editor?.dispose();
+	});
 
-    export { getEditorValue };
+	export { getEditorValue };
 </script>
 
 <div class="h-full" bind:this={editorParent}>
-    <div class="h-full" bind:this={editorContainer} />
+	<div class="h-full" bind:this={editorContainer} />
 </div>
--- website/src/routes/api/code-eval/+server.ts
+++ website/src/routes/api/code-eval/+server.ts
@@ -1,29 +1,29 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import { error } from '@sveltejs/kit';
 
-export type Language = "Lua";
+export type Language = 'Lua';
 
 export async function POST({ request }: RequestEvent): Promise<Response> {
-  const url = import.meta.env.VITE_CODERUNNER_BACKEND_URL as string;
-  const api = import.meta.env.VITE_CODERUNNER_BACKEND_API_PATH as string;
-  if (!url || !api) {
-    console.log("Coderunner backend API URL is not set");
-    throw error(400, 'Internal server error');
-  }
-  const body = await request.text();
+	const url = import.meta.env.VITE_CODERUNNER_BACKEND_URL as string;
+	const api = import.meta.env.VITE_CODERUNNER_BACKEND_API_PATH as string;
+	if (!url || !api) {
+		console.log('Coderunner backend API URL is not set');
+		throw error(400, 'Internal server error');
+	}
+	const body = await request.text();
 
-  try {
-    const response = await fetch(`${url}${api}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      },
-      body,
-    }); 
-    return response;
-  } catch (e) {
-    console.log("Failed to compile code: ", e);
-    throw error(500, `Internal server error`);
-  }
+	try {
+		const response = await fetch(`${url}${api}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			},
+			body
+		});
+		return response;
+	} catch (e) {
+		console.log('Failed to compile code: ', e);
+		throw error(500, `Internal server error`);
+	}
 }
