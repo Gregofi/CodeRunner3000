1ff91f1f16df247664e5a2487bef6d907181fd06
gregofi1 <filip.gregor98@gmail.com>
2024-05-29 18:34:15 +0000

    feat: add language select boxes

---
+++ website2.0/src/app/lib/languages.ts
@@ -0,0 +1,46 @@
+export type Executor = {
+    // Server name of the interpreter
+    value: string,
+    // Display name of the interpreter
+    label: string,
+}
+
+export type LanguageConfig = {
+    name: string,
+    label: string,
+    server_name: string
+    interpreters?: Executor[],
+    compilers?: Executor[],
+}
+
+export const languages: Record<string, LanguageConfig> = {
+    "lua": {
+        name: 'lua',
+        label: 'Lua',
+        server_name: 'lua',
+        interpreters: [
+            { value: 'lua5.1.5', label: '5.1' },
+            { value: 'lua5.2.4', label: '5.2' },
+            { value: 'lua5.3.6', label: '5.3' },
+            { value: 'lua5.4.6', label: '5.4' },
+        ],
+    },
+    "python": {
+        name: 'python',
+        label: 'Python',
+        server_name: 'python',
+        interpreters: [
+            { value: 'python-bookworm', label: 'Debian Bookworm' },
+        ],
+    },
+    "cpp": {
+        name: 'cpp',
+        label: 'C++',
+        server_name: 'cpp',
+        compilers: [
+            { value: 'gcc-bookworm', label: 'GCC - Debian Bookworm' },
+        ],
+    },
+}
+
+export type Language = keyof typeof languages;
--- website2.0/src/app/page.tsx
+++ website2.0/src/app/page.tsx
@@ -3,30 +3,52 @@
 import { Header } from './components/header';
 import { CodeOutput } from './components/codeOutput';
 
+import { languages, Language } from './lib/languages';
+
 import Editor from '@monaco-editor/react';
+import React from 'react';
+import Select from 'react-select';
+
+type CurrentChoice = {
+    name: Language,
+    interpreter?: string,
+    compiler?: string,
+}
 
 export default function Home() {
-  return (
-    <div className="flex h-full flex-col">
-      <Header />
-      <main className="flex flex-col grow">
-        <div className="grow flex">
-            <div>
-                
-            </div>
-            <div className="grow">
-                <Editor
-                    height="100%"
-                    defaultLanguage="javascript"
-                    defaultValue="// some comment"
-                />
-            </div>
-        </div>
-        <div className="flex flex-col h-1/3">
-            <CodeOutput code="console.log('Hello, world!');" />
-            <CodeOutput code="console.log('Hello, world!');" />
-        </div>
-      </main>
-    </div>
-  );
+    // TODO: Maybe make this an object (migrate langObject to this)
+    let [lang, setLang] = React.useState<CurrentChoice>({ name: 'lua', interpreter: "lua5.4.6" });
+    const langObject = languages[lang.name];
+    const currentInterpreter = langObject?.interpreters?.find((i) => i.value === lang.interpreter);
+    const currentCompiler = langObject?.compilers?.find((i) => i.value === lang.compiler);
+
+    if (!langObject) {
+        return <div>Language not found</div>;
+    }
+
+    return (
+      <div className="flex h-full flex-col">
+        <Header />
+        <main className="flex flex-col grow">
+          <div className="grow flex flex-col">
+              <div className="flex flex-row">
+                  <Select value={langObject} getOptionValue={o => o.name} options={Object.values(languages)} onChange={(conf) => setLang({ ...lang, name: conf!.name, interpreter: conf!.interpreters?.at(0)?.value, compiler: conf!.compilers?.at(0)?.value })}/>
+                  {currentInterpreter && <Select value={currentInterpreter} options={langObject.interpreters} onChange={(opt) => setLang({ ...lang, interpreter: opt!.value })}/>}
+                  {currentCompiler && <Select value={currentCompiler} options={langObject.compilers} onChange={(opt) => setLang({ ...lang, compiler: opt!.value })}/>}
+              </div>
+              <div className="grow">
+                  <Editor
+                      height="100%"
+                      defaultLanguage="javascript"
+                      defaultValue="// some comment"
+                  />
+              </div>
+          </div>
+          <div className="flex flex-col h-1/3">
+              <CodeOutput code="console.log('Hello, world!');" />
+              <CodeOutput code="console.log('Hello, world!');" />
+          </div>
+        </main>
+      </div>
+    );
 }
