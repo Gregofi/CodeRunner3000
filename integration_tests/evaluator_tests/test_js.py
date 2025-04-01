0350341ee6583df2f3c20e212acce689cc7366fc
Filip Gregor <filip.gregor98@gmail.com>
2025-04-01 22:24:48 +0200

    test: add simple nodejs test

---
+++ integration_tests/evaluator_tests/test_js.py
@@ -0,0 +1,16 @@
+import requests
+
+EVALUATOR_ADDRESS = "http://evaluator:7800/api/v1/evaluate"
+
+def generate_python(code: str):
+    return {"language": "js", "code": code, "executor": "nodejs-bookworm"}
+
+def test_simple_python():
+    payload = generate_python("""
+console.log("Hello, World!");
+""")
+    response = requests.post(EVALUATOR_ADDRESS, json=payload)
+    assert response.status_code == 200
+    values = response.json()
+    assert values["stdout"] == "Hello, World!\n"
+    assert values["stderr"] == ""
