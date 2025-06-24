77aa1c23e1d3a3782ae48b103680bc67c056b846
Filip Gregor <filip.gregor98@gmail.com>
2025-06-24 22:39:05 +0200

    feat: add structures in glushkov, responsive

---
+++ website/src/routes/algorithms/AlgoCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  const {
+    name,
+    description,
+    path: link,
+  }: {
+    name: string;
+    description: string;
+    path: string;
+  } = $props();
+</script>
+
+<a
+  class="p-4 mt-4 border border-gray-200 rounded-lg shadow-md cursor-pointer hover:bg-gray-50 transition-colors"
+  href={`algorithms/${link}`}
+>
+  <div><h3 class="font-bold text-lg">{name}</h3></div>
+  <p class="algo-description">{description}</p>
+</a>
---
+++ website/src/routes/algorithms/regexes/GlushkovPair.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import type { GlState } from "@gregofi1/regex-tooling";
+  const { pair }: { pair: GlState } = $props();
+</script>
+
+<span>{pair[0]}</span><span class="text-sm align-bottom">{pair[1]}</span>
